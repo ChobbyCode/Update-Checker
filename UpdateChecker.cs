9f2425ad06b8545d159fbf6a26e0657314a06e51
Jacob Hadfield <jacobhadfield99@gmail.com>
2023-08-31 21:28:28 +0100

    Move

--- UpdateChecker.cs
+++ UpdateChecker.cs
@@ -37,7 +37,7 @@ namespace Update_Checker
             }
         }
 
-        public async Task<int> GetVersionPart(int part)
+        public async Task<int> GetVersionPart(int part, string url)
         {
             // Creates a new http client
             using (HttpClient client = new HttpClient())
@@ -46,7 +46,7 @@ namespace Update_Checker
                 client.DefaultRequestHeaders.Add("User-Agent", "ChobbyCodeUpdateChecker");
 
                 //Passes the url to the http client and does stuff
-                using (HttpResponseMessage response = await client.GetAsync(@"https://api.github.com/repos/microsoft/artifacts-credprovider/releases/latest"))
+                using (HttpResponseMessage response = await client.GetAsync(url))
                 {
 
                     using (HttpContent content = response.Content)
@@ -54,10 +54,104 @@ namespace Update_Checker
                         string myContent = await content.ReadAsStringAsync();
                         JSONStruc info = JsonConvert.DeserializeObject<JSONStruc>(myContent);
 
+                        string versionTag = info!.tag_name + ".";
+
+                        string letter = "";
+                        string word = "";
+                        int charLet = 1;
+                        while(letter != ".")
+                        {
+                            word = word + letter;
+
+                            letter = versionTag[charLet].ToString();
+                            charLet += 1;
+                        }
+                        string MainVersion = word;
+
+                        word = "";
+                        letter = "";
+                        while (letter != ".")
+                        {
+                            word = word + letter;
+
+                            letter = versionTag[charLet].ToString();
+                            charLet += 1;
+                        }
+                        string MinorVersion = word;
+
+                        word = "";
+                        letter = "";
+                        while (letter != ".")
+                        {
+                            word = word + letter;
+
+                            letter = versionTag[charLet].ToString();
+                            charLet += 1;
+                        }
+                        string BuildVersion = word;
+
+                        if(part == 0)
+                        {
+                            try
+                            {
+                                return Convert.ToInt16(MainVersion);
+                            }
+                            catch
+                            {
+                                return -1;
+                            }
+                        }else if (part == 1)
+                        {
+                            try
+                            {
+                                return Convert.ToInt16(MinorVersion);
+                            }
+                            catch
+                            {
+                                return -1;
+                            }
+                        }else if (part == 2)
+                        {
+                            try
+                            {
+                                return Convert.ToInt16(BuildVersion); ;
+                            }
+                            catch
+                            {
+                                return -1;
+                            }
+                        }
+                        else
+                        {
+                            return -1;
+                        }
+                    }
+                }
+            }
+        }
+
+        public async Task<bool> CheckForUpdates(string currentVersion, string url)
+        {
+            // Creates a new http client
+            using (HttpClient client = new HttpClient())
+            {
+
+                client.DefaultRequestHeaders.Add("User-Agent", "ChobbyCodeUpdateChecker");
+
+                //Passes the url to the http client and does stuff
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+
+                    using (HttpContent content = response.Content)
+                    {
+                        // Reads
+                        string myContent = await content.ReadAsStringAsync();
+                        JSONStruc info = JsonConvert.DeserializeObject<JSONStruc>(myContent);
+
                         string versionTag = info!.tag_name;
 
-
-                        return 1;
+                        //Returns the version
+                        return versionTag;
 
                     }
                 }
