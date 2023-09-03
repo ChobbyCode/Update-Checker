b1dc716a0933882cb680353119a8b34682ae98a7
ChobbyCode <jacobhadfield99@gmail.com>
2023-09-03 14:36:25 +0100

    Chill Support

---
+++ DownloadManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace Update_Checker
+{
+    public class DownloadManager
+    {
+        public void Download()
+        {
+            // Starts Chill Updator
+            Process process = new Process();
+
+            process.StartInfo.FileName = AppDomain.CurrentDomain.BaseDirectory + @"Chill\UC-Update-Downloader.exe";
+            process.Start();
+
+            // Closes the application
+            System.Environment.Exit(0);
+
+        }
+    }
+}
